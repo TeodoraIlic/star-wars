2d235ff79fbdb87d8607a1ebc85e6db90e24614b
Teodora Ilic <te.ilic@levi9.com>
2020-05-21 12:50:43 +0200

    in film component vehicles button logic done

--- src/app/overview-page/vehicles/vehicles.component.ts
+++ src/app/overview-page/vehicles/vehicles.component.ts
@@ -5,6 +5,7 @@ import { Vehicle } from './vehicles.model';
 import { CategoriesService } from 'src/app/service/categories.service';
 import { VehiclesService } from 'src/app/service/vehicles.service';
 import { FuzzySearchService } from 'src/app/service/fuzzy-search.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-vehicles',
@@ -13,7 +14,10 @@ import { FuzzySearchService } from 'src/app/service/fuzzy-search.service';
 })
 export class VehiclesComponent implements OnInit, OnDestroy {
   isLoading = true;
-  vehiclesUrl: string; 
+  vehiclesUrl: string;
+  viewAll: boolean = true;
+  idList: string[]; 
+  listOfVehiclesToView = [];
   searchValue: string = '';
   categories: Categories;
   categoriesSub: Subscription;
@@ -21,9 +25,29 @@ export class VehiclesComponent implements OnInit, OnDestroy {
   vehiclesSub: Subscription;
   vehicle: Vehicle;
   vehicleSub: Subscription;
-  constructor(private categoriesService: CategoriesService, private vehiclesService: VehiclesService, private fuzzySearchService: FuzzySearchService) { }
+  constructor(private categoriesService: CategoriesService, 
+              private vehiclesService: VehiclesService, 
+              private fuzzySearchService: FuzzySearchService,
+              private router: Router,
+              private route: ActivatedRoute) {
+                if(this.router.getCurrentNavigation()){
+                  this.idList =  this.router.getCurrentNavigation().extras.state.vehicles;
+  
+                 }
+               }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap)=>{
+      if(paramMap.has('related')){
+          this.viewAll = false ;
+          this.getRelatedVehicles(this.idList); 
+      }
+    });
+    this.vehiclesService.getVehicleList();
+    this.vehicleSub = this.vehiclesService.getVehicleUpdateListener().subscribe( vehicleList =>{
+      this.listOfVehiclesToView = vehicleList;
+    });
+
     this.categoriesService.getCategories();
     this.categoriesSub = this.categoriesService.getCategoriesUpdateListener().subscribe((categories)=>{
       this.isLoading = false;
@@ -45,6 +69,11 @@ export class VehiclesComponent implements OnInit, OnDestroy {
     this.vehiclesService.getVehicles(url);
     this.vehiclesSub = this.vehiclesService.getVehiclesUpdateListener().subscribe((starships)=>{
       this.vehicles = starships;
+    });
+  }
+  getRelatedVehicles(idList: string[]){
+    idList.map(id => {
+      this.vehiclesService.getVehicle(id);
     });
   }
 
